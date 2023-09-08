62ec7094b8dcf762dab526b4845d6b880163fdbc
DESKTOP-EL54GVD\USER <onyebuchiaja@gmail.com>
2023-09-08 10:45:29 +0100

    create custome exception

--- src/main/java/com/springbootblog/repository/PostRepository.java
+++ src/main/java/com/springbootblog/repository/PostRepository.java
@@ -2,6 +2,8 @@ package com.springbootblog.repository;
 
 import com.springbootblog.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
