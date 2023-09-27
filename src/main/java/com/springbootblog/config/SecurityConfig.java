94a842a8ac1b2efbb2282a1850e11786470d9836
DESKTOP-EL54GVD\USER <onyebuchiaja@gmail.com>
2023-09-27 17:02:55 +0100

    implement basic authorization

---
+++ src/main/java/com/springbootblog/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.springbootblog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        //basic authentication
+        http.csrf().disable()
+                .authorizeHttpRequests((authorize)->authorize.anyRequest().authenticated()
+                        ).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
