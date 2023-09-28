e7b4b3509fc11cbf7e053033e2016417c75ebc0f
aja onyebuchi <aja.onyebuchi@yahoo.com>
2023-09-28 13:00:04 +0100

    create CustomUserDetailsService class to load user from database

--- src/main/java/com/springbootblog/repository/RoleRepository.java
+++ src/main/java/com/springbootblog/repository/RoleRepository.java
@@ -7,5 +7,4 @@ import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(String name);
-//    Optional<Role> findByName(String name);
 }
---
+++ src/main/java/com/springbootblog/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.springbootblog.security;
+
+import com.springbootblog.entity.User;
+import com.springbootblog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+@Service
+public class CustomUserDetailsService implements UserDetailsService {   //loading usernameOrEmail from database (database authentication)
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(()->new UsernameNotFoundException("User with username or email" + usernameOrEmail + "not found"));
+
+        //convert user to a spring security provided user object
+        Set<GrantedAuthority> authorities = user.getRoles().stream().map((role)-> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toSet());
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+    }
+}
