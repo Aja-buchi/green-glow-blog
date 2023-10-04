9950e2e7623132f2fa915ca3aea6a75c4e5c7541
aja onyebuchi <aja.onyebuchi@yahoo.com>
2023-10-04 16:26:30 +0100

    Customize swagger models documentation with annotations

--- src/main/java/com/springbootblog/payload/PostDto.java
+++ src/main/java/com/springbootblog/payload/PostDto.java
@@ -1,5 +1,6 @@
 package com.springbootblog.payload;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -9,19 +10,35 @@ import lombok.NoArgsConstructor;
 import java.util.Set;
 
 @Data @AllArgsConstructor @NoArgsConstructor
+//Customizes swagger API documentation
+@Schema(
+        description = "PostDto Model Information"
+)
 public class PostDto {
     private Long id;
 
     //applying validations to fields
     @NotEmpty
     @Size(min = 2, message = "Post title should have atleast 2 characters")
+    //Customizes swagger API documentation
+    @Schema(
+            description = "Blog Post Title"
+    )
     private String title;
 
     @NotEmpty
     @Size(min = 10, message = "Post description should have atleast 10 characters")
+    //Customizes swagger API documentation
+    @Schema(
+            description = "Blog Post Description"
+    )
     private String description;
 
     @NotEmpty
+    //Customizes swagger API documentation
+    @Schema(
+            description = "Blog Post Content"
+    )
     private String content;
 
     //including sets of comments when fetching posts
