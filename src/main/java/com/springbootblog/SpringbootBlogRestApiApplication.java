a842d31a803b20aff71715bc8a5abda3bdebad18
aja onyebuchi <aja.onyebuchi@yahoo.com>
2023-10-04 12:32:52 +0100

    defining general API information (using annotations)

--- src/main/java/com/springbootblog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springbootblog/SpringbootBlogRestApiApplication.java
@@ -1,11 +1,37 @@
 package com.springbootblog;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+//defining general swagger openAPI information (using annotations)
+@OpenAPIDefinition(
+		info = @Info(
+				title = "SpringBoot Blog REST APIs",
+				description = "SpringBoot Blog REST APIs Documentations",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Onyebuchi",
+						email = "onyebuchiaja@gmail.com",
+						url = "https://www.linkedin.com/in/onyebuchi-aja/"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.linkedin.com/in/onyebuchi-aja/"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "SpringBoot Blog APP Documentations",
+				url = "https://github.com/Aja-buchi/green-glow-blog"
+		)
+)
 public class SpringbootBlogRestApiApplication {
 
 	//Using ModelMapper
