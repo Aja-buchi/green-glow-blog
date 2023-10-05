910ec1d2bc19373c928642d0728e076e0d38c276
aja onyebuchi <aja.onyebuchi@yahoo.com>
2023-10-05 15:31:29 +0100

    write a code to insert metadata tables for AWS deployment

--- src/main/java/com/springbootblog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springbootblog/SpringbootBlogRestApiApplication.java
@@ -1,11 +1,15 @@
 package com.springbootblog;
 
+import com.springbootblog.entity.Role;
+import com.springbootblog.repository.RoleRepository;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -32,7 +36,8 @@ import org.springframework.context.annotation.Bean;
 				url = "https://github.com/Aja-buchi/green-glow-blog"
 		)
 )
-public class SpringbootBlogRestApiApplication {
+//implementing commandlineRunner allows you write a code to insert metadata tables for AWS deployment
+public class SpringbootBlogRestApiApplication implements CommandLineRunner {
 
 	//Using ModelMapper
 	@Bean
@@ -42,5 +47,18 @@ public class SpringbootBlogRestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
 	}
+	@Autowired
+	private RoleRepository roleRepository;
 
+//	write a code to insert metadata tables for AWS deployment
+	@Override
+	public void run(String... args) throws Exception {
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN2");
+		roleRepository.save(adminRole);
+
+		Role userRole = new Role();
+		userRole.setName("USER_ROLE2");
+		roleRepository.save(userRole);
+	}
 }
